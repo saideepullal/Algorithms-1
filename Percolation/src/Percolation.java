6d5893593ec58387b813f8e4f53aca0623184887
Saideep Ullal <saideepullal.ullal@gmail.com>
2020-02-10 19:46:50 +0530

    Added more logic to Percolation and PercolationStats
    Failing 15/100

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,41 +1,108 @@
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-	
-	public  Percolation(int n) {
+
+	private boolean grid[][];
+	private int maxSize;
+	private WeightedQuickUnionUF weightedQuickUnionUF;
+	private int bottomCommonPoint;
+	private int topCommonPoint = 0;
+	private int noOfOpenSites = 0;
+
+	public Percolation(int n) {
 		// TODO add constructor that initializes the 2x2 matrix for percolation
+		maxSize = n;
+		bottomCommonPoint = (n * n + 1);
+		// topCommonPoint = 0;
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
+		grid = new boolean[n][n];
+		weightedQuickUnionUF = new WeightedQuickUnionUF(n * n + 2);
+
 	}
-	 // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {
-    	
-    }
 
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
-		return false;
-    	
-    }
+	// opens the site (row, col) if it is not open already
+	public void open(int row, int col) {
+		if (row > maxSize || row < 1 || col > maxSize || col < 1) {
+			throw new IllegalArgumentException();
+		}
+		if (row == 1) {
+			int weightedQuickUnionGridIndex = getWeightedUnionGridIndex(row, col);
+			weightedQuickUnionUF.union(weightedQuickUnionGridIndex, topCommonPoint);
+		}
+		if (row == maxSize) {
+			int weightedQuickUnionGridIndex = getWeightedUnionGridIndex(row, col);
+			weightedQuickUnionUF.union(weightedQuickUnionGridIndex, bottomCommonPoint);
+		}
+		if (!isOpen(row, col)) {
+			int weightedQuickUnionGridIndex = getWeightedUnionGridIndex(row, col);
+			performConnectIfOpen(weightedQuickUnionGridIndex, row + 1, col);
+			performConnectIfOpen(weightedQuickUnionGridIndex, row - 1, col);
+			performConnectIfOpen(weightedQuickUnionGridIndex, row, col + 1);
+			performConnectIfOpen(weightedQuickUnionGridIndex, row, col - 1);
+			grid[row - 1][col - 1] = true;
+			noOfOpenSites++;
+		}
 
-    // is the site (row, col) full?
-    public boolean isFull(int row, int col) {
-		return false;
-    	
-    }
+	}
 
-    // returns the number of open sites
-    public int numberOfOpenSites() {
-		return 0;
-    	
-    }
+	// is the site (row, col) open?
+	public boolean isOpen(int row, int col) {
+		if (row > maxSize || row < 1 || col > maxSize || col < 1) {
+			throw new IllegalArgumentException();
+		}
+		return grid[row - 1][col - 1];
 
-    // does the system percolate?
-    public boolean percolates() {
-		return false;
-    	
-    }
+	}
 
-    // test client (optional)
-    public static void main(String[] args) {
-    	
-    }
+	// is the site (row, col) full?
+	public boolean isFull(int row, int col) {
+		if (row > maxSize || row < 1 || col > maxSize || col < 1) {
+			throw new IllegalArgumentException();
+		}
+		int weightedIndex = getWeightedUnionGridIndex(row, col);
+		if (weightedQuickUnionUF.find(weightedIndex) == weightedQuickUnionUF.find(topCommonPoint)) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	// returns the number of open sites
+	public int numberOfOpenSites() {
+		return noOfOpenSites;
+
+	}
+
+	// does the system percolate?
+	public boolean percolates() {
+		return weightedQuickUnionUF.find(topCommonPoint) == weightedQuickUnionUF.find(bottomCommonPoint);
+
+	}
+
+	// test client (optional)
+	public static void main(String[] args) {
+
+	}
+
+	private void performConnectIfOpen(int WQFgridIndex, int row, int column) {
+		try {
+			if (isOpen(row, column)) {
+				int consecutiveIndex = getWeightedUnionGridIndex(row, column);
+				weightedQuickUnionUF.union(WQFgridIndex, consecutiveIndex);
+			}
+
+		} catch (IndexOutOfBoundsException e) {
+
+		}
+
+	}
+
+	private int getWeightedUnionGridIndex(int row, int column) {
+		return (row - 1) * maxSize + column;
+	}
 
 }
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -1,37 +1,74 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
 
-    // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) {
-    	
-    }
+	private double mean;
+	private double stddev;
+	private double confidenceLo;
+	private double confidenceHi;
 
-    // sample mean of percolation threshold
-    public double mean() {
-		return 0;
-    	
-    }
+	private int noOfOpenCells;
 
-    // sample standard deviation of percolation threshold
-    public double stddev() {
-		return 0;
-    	
-    }
+	// perform independent trials on an n-by-n grid
+	public PercolationStats(int n, int trials) {
 
-    // low endpoint of 95% confidence interval
-    public double confidenceLo() {
-		return 0;
-    	
-    }
+		if (n <= 0 || trials <= 0) {
+			throw new IllegalArgumentException();
+		}
+		double[] percolationTresholdList = new double[trials];
+		for (int i = 0; i < trials; i++) {
+			Percolation percolation = new Percolation(n);
+			while (!percolation.percolates()) {
+				int row = StdRandom.uniform(n);
+				int column = StdRandom.uniform(n);
+				percolation.open(row, column);
+				noOfOpenCells = percolation.numberOfOpenSites();
+			}
+			percolationTresholdList[i] = noOfOpenCells / (n * n);
+			mean = StdStats.mean(percolationTresholdList);
+			stddev = StdStats.stddev(percolationTresholdList);
+			confidenceLo = mean - ((1.96 * stddev) / (Math.sqrt(trials)));
+			confidenceLo = mean + ((1.96 * stddev) / (Math.sqrt(trials)));
 
-    // high endpoint of 95% confidence interval
-    public double confidenceHi() {
-		return 0;
-    	
-    }
+		}
 
-   // test client (see below)
-   public static void main(String[] args) {
-	   
-   }
+	}
+
+	// sample mean of percolation threshold
+	public double mean() {
+		return mean;
+
+	}
+
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+		return stddev;
+
+	}
+
+	// low endpoint of 95% confidence interval
+	public double confidenceLo() {
+		return confidenceLo;
+
+	}
+
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		return confidenceHi;
+
+	}
+
+	// test client (see below)
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		int trials = Integer.parseInt(args[1]);
+
+		PercolationStats stats = new PercolationStats(n, trials);
+		System.out.println(stats.mean());
+		System.out.println(stats.stddev());
+		System.out.println(stats.confidenceLo());
+		System.out.println(stats.confidenceHi());
+	}
 
 }
