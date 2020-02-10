381ea3716cca1f546eb30039595b632b539b2d42
Saideep Ullal <saideepullal.ullal@gmail.com>
2020-02-10 13:05:10 +0530

    Added stub with suitable comments
    PercolationVisualizer and InteractivePercolationVisualizer as written by Robert Sedgewick and Kevin Wayne

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,4 +1,41 @@
 
 public class Percolation {
+	
+	public  Percolation(int n) {
+		// TODO add constructor that initializes the 2x2 matrix for percolation
+	}
+	 // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+    	
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+		return false;
+    	
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+		return false;
+    	
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+		return 0;
+    	
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+		return false;
+    	
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+    	
+    }
 
 }
